4cc2b9b46af739acc88e876d8ea0d2fc9550eac4
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2024-03-26 16:43:16 -0300

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel= 60.5
+
+console.log(`Sua velocidade é de ${vel}km/h`)
+// CONDIÇÃO SIMPLES
+if(vel>60){
+    console.log(`Você passou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre com sinto de segurança.`)
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var country= "EUA"
+
+// CONDIÇÃO COMPOSTA
+if(country == "BR"){
+    console.log("Você é brasileiro!")
+}
+else{
+    console.log("Você é estrangeiro.")
+}

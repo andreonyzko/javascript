8c61b6bdb28627922868fd2add61bae6b971ea60
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2024-03-28 16:31:38 -0300

    .

--- aula12ex/ex015/js/script.js
+++ aula12ex/ex015/js/script.js
@@ -4,17 +4,21 @@ function carregar() {
     var body= document.querySelector('body')
     var now= new Date()
     // var hour= now.getHours()
-    var hour=3
-    msgfield.innerHTML = `<p>Agora são ${hour} horas</p>`
+    var hour= 1
+    var minutes= now.getMinutes()
+    msgfield.innerHTML = `<p>Agora são ${hour}h${minutes}m</p>`
 
-    if (hour<12){
+    if (hour>0 && hour<12){
         imgfield.innerHTML = `<img src="media/manha.jpg" alt="fotodamanha">`
         body.style.background= '#BFC7C8'
     } else if(hour<=18){
         imgfield.innerHTML = `<img src="media/tarde.jpg" alt="fotodamanha">`
         body.style.background= '#3C2B41'
-    } else{
+    } else if(hour>18 && hour<=23){
         imgfield.innerHTML = `<img src="media/noite.jpg" alt="fotodamanha">`
         body.style.background= 'black'
+    } else{
+        msgfield.innerHTML = `<p>ERRO: hora invalida</p>`
+        imgfield.innerHTML= ``
     }
 }

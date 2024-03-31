e0c16c22721e290617a584d8464f42d3327a9f53
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2024-03-31 20:46:06 -0300

    aula12ex/ex016

--- aula12ex/ex016/js/script.js
+++ aula12ex/ex016/js/script.js
@@ -1,39 +1,32 @@
 function verify() {
-    var yearbirth= Number(document.querySelector('#yearbirth').value)
-    var datenow = new Date()
-    var yearnow= datenow.getFullYear()
-    var age= yearnow-yearbirth
-    var gender= document.getElementsByName('gender')
-    var resultcontainer= document.querySelector('#result-container')
-    var txtresult= document.querySelector('#result-container p')
-    var imgresult= document.createElement('img')
-    resultcontainer.appendChild(imgresult)
-
+    var yearbirth = Number(document.querySelector('#yearbirth').value)
+    var currentyear= new Date().getFullYear()
+    var age= currentyear-yearbirth
     if(age<0){
-        alert("[ERRO] Idade inválida.")
-    } else {
+        alert('[ERRO] ano de nascimento futuro')
+    } else{
+        var gender= document.getElementsByName('gender')
+        var imgresult= document.querySelector('#container-result img')
+        var txtresult= document.querySelector('#container-result p')
+        var genderresult= ''
         if(gender[0].checked){
-            txtresult.innerText= `Encontramos um homem de ${age} anos.`
-            if(age>=0 && age<=10){
-                imgresult.setAttribute('src','media/crianca-mas.jpg')
-            } else if(age<20){
-                imgresult.setAttribute('src','media/jovem-mas.jpg')
-            } else if(age<50){
-                imgresult.setAttribute('src','media/adulto-mas.jpg')
-            } else{
-                imgresult.setAttribute('src','media/idoso-mas.jpg')
-            }
+            gender = 'mas'
+            genderresult= 'um homem'
         } else if(gender[1].checked){
-            txtresult.innerText= `Encontramos uma mulher de ${age} anos.`
-            if(age>=0 && age<=10){
-                imgresult.setAttribute('src','media/crianca-fem.jpg')
-            } else if(age<20){
-                imgresult.setAttribute('src','media/jovem-fem.jpg')
-            } else if(age<50){
-                imgresult.setAttribute('src','media/adulto-fem.jpg')
-            } else{
-                imgresult.setAttribute('src','media/idoso-fem.jpg')
-            }
+            gender= 'fem'
+            genderresult= 'uma mulher'
+        }
+
+        txtresult.innerText= `Encontramos ${genderresult} de ${age} anos.`
+        
+        if (age<10){
+            imgresult.src = `media/crianca-${gender}.jpg`
+        } else if(age < 20){
+            imgresult.src= `media/jovem-${gender}.jpg`
+        } else if(age<50){
+            imgresult.src = `media/adulto-${gender}.jpg`
+        } else {
+            imgresult.src= `media/idoso-${gender}.jpg`
         }
     }
 }
